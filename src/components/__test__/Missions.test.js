1098361b029627c14ef608c997a9f6242d4cd1ff
EyasuTeshome <joshtesh91@mail.com>
2022-09-02 23:34:41 +0800

    fixed problems on test and more

--- src/components/__test__/Missions.test.js
+++ src/components/__test__/Missions.test.js
@@ -1,51 +1,69 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import store from '../redux/configureStore';
-import { fetchingData } from '../redux/missions/missions';
-import Mission from './Mission';
+import configureStore from 'redux-mock-store';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
 
-const Missions = () => {
-  const dispatch = useDispatch();
+import Rockets from '../__mock__/Rockets';
+import {
+  joinMission,
+  leaveMission,
+  storeMissions,
+} from '../../redux/missions/missions';
 
-  useEffect(() => {
-    if (!(store.getState().missionsReducer.length > 0)) {
-      dispatch(fetchingData());
-    }
-  }, []);
+describe('Dispatch actions tests', () => {
+  const middlewares = [];
+  const mockStore = configureStore(middlewares);
 
-  const isMobile = () => window.innerWidth < 768;
+  it('should dispatch Reserve Rocket action', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
 
-  const missionsData = useSelector((state) => state.missionsReducer);
+    // Dispatch the action
+    store.dispatch(joinMission());
 
-  return (
-    <div className="p-10">
-      <h2 className="text-xl mb-6 md:text-4xl font-semibold font-serif uppercase">
-        AVailable Missions
-      </h2>
-      <div className="hidden md:block">
-        <div className="grid grid-cols-12 border border-black auto-cols-min font-bold text-xl">
-          <div className="p-2 border-r-black col-span-2 border-r">Mission</div>
-          <div className="p-2 border-r-black col-span-6 border-r">
-            Description
-          </div>
-          <div className="p-2 border-r-black col-span-2 border-r">Status</div>
-          <div className="p-2 col-span-2">Action</div>
-        </div>
-        {missionsData[0]
-          && missionsData[0].map((mission, index) => (
-            <Mission key={mission.id} mission={mission} index={index} />
-          ))}
-      </div>
-      {isMobile() && (
-        <div className="flex flex-col gap-6">
-          {missionsData[0]
-            && missionsData[0].map((mission, index) => (
-              <Mission key={mission.id} mission={mission} index={index} />
-            ))}
-        </div>
-      )}
-    </div>
-  );
-};
+    // Test if your store dispatched the expected actions
+    const actions = store.getActions();
+    const expectedPayload = { type: 'JOIN_MISSION' };
+    expect(actions).toEqual([expectedPayload]);
+  });
 
-export default Missions;
+  it('should dispatch cancel Reservation action', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+
+    // Dispatch the action
+    store.dispatch(leaveMission());
+
+    // Test if your store dispatched the expected actions
+    const actions = store.getActions();
+    const expectedPayload = { type: 'LEAVE_MISSION' };
+    expect(actions).toEqual([expectedPayload]);
+  });
+
+  it('should dispatch store Rockets action', () => {
+    const initialState = {};
+    const store = mockStore(initialState);
+
+    // Dispatch the action
+    store.dispatch(storeMissions());
+
+    // Test if your store dispatched the expected actions
+    const actions = store.getActions();
+    const expectedPayload = { type: 'STORE' };
+    expect(actions).toEqual([expectedPayload]);
+  });
+});
+
+describe('Missions component snapshot tests', () => {
+  const mockStore = configureStore();
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={mockStore({})}>
+          <Rockets />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
